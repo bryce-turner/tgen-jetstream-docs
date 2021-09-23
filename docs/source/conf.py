e4a5eb107821b8e17b4a8a8910a7a460d2c0f353
Bryce Turner <40504111+bryce-turner@users.noreply.github.com>
2021-09-23 16:01:38 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'TGen Jetstream Docs'
+copyright = '2021, TGen'
+author = 'bryce-turner'
 
 release = '0.1'
 version = '0.1.0'
