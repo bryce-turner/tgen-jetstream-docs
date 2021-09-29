99e36d5eae6f78636eea9d78c91b804236a1cfc8
bryce-turner <bturner@tgen.org>
2021-09-29 15:26:21 -0700

    Updates for copybutton

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,3 +38,9 @@ html_theme_options = {
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# -- Copybutton Configuration
+copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
+copybutton_prompt_is_regexp = True
+copybutton_line_continuation_character = "\\"
+copybutton_here_doc_delimiter = "EOT"
